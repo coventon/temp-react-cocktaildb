c97aac77508b1535dff5c73548188c50308a28d5
Roma <rnejuta@gmail.com>
2022-03-02 01:32:27 -0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, velit provident laudantium consequatur odio vero blanditiis? Nihil soluta aspernatur aperiam maiores sunt repellat eos commodi quaerat labore expedita unde, ducimus ullam doloremque consectetur maxime ut et repellendus optio in debitis, fuga error reprehenderit omnis.</p>
+    </section>
+  )
+}
+
+export default About
